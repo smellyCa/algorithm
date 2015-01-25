09d02de66aa6541696fe2a02163a64b85f0b2040
SQRL <429060098@qq.com>
2015-01-24 22:38:29 -0600

    23_11-2_sortString

---
+++ solutions/23_11-2_sortStringWithAnagramsNext2EachOther.cpp
@@ -0,0 +1,41 @@
+//write code to remove duplicates from an unsorted linked list
+
+#include <iostream.h>
+#include <hash_map.h>
+using namespace std;
+
+typedef struct _node{
+	int data;
+	struct _node* next;
+}Node;
+
+typedef struct _list{
+	Node* list;
+}List;
+
+//I didn't implement these three functions.
+char* sortString(char* string);
+void addToLinkedList(List* list,char* string);
+List* createLinkedList();
+
+void sort(char* string[]){
+	hash_map<char*,List*> set;
+	int i=0;
+	//group words by anagram
+	while(string[i]!='\0'){
+		//sort the string
+		char* key=sortString(string[i]);
+		//if not in the hash map
+		if(set.find(key)==set.end()){
+			set[key]=createLinkedList();
+		}
+		addToLinkedList(set[key],string[i]);
+		i++;
+	}
+	//convert hash map to array
+	i=0;
+	for(hash_map<char*,List*>::iterator it=set.begin();it!=set.end();set++){
+		string[i]=it->list->data;
+		i++;
+	}
+}
